9de7cbc11b718f5cd82ddde4b32f7f50beac1118
João Gabriel Grandotto Viana <63874169+joaograndotto@users.noreply.github.com>
2022-07-25 13:25:30 -0300

    Add files via upload

---
+++ includes/rodape.php
@@ -0,0 +1,36 @@
+<?php
+?>
+  <!-- Grid container -->
+  <div class="container p-4 mt-3 nb ">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <footer class="text-center text-white">
+      <!-- Grid container -->
+      <div class="container p-4"></div>
+      <!-- Grid container -->
+
+      <!-- Copyright -->
+      <div class="text-center p-3">
+        <h5 style="font-weight: bold;">Instituto Federal de Educação, Ciência e Tecnologia de Goiás</h5>
+        <p>Av. Assis Chateaubriand, nº 1.658, Setor Oeste,  
+          <br>Goiânia, Goiás, Brasil,
+          <br>CEP: 74130-012</p>
+        © 2022: (DJLO)
+      </div>
+      <!-- Copyright -->
+    </footer>
+      <!--Grid column-->
+
+      
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <script src='js/bootstrap.bundle.js'></script>
+
+    
+</body>
+</html>
---
+++ includes/topo.php
@@ -0,0 +1,47 @@
+<?php 
+?>
+<html>
+
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>Coopercred Implementos Rodoviários</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' type='text/css' media='screen' href='css/bootstrap.css'>
+  <link rel='stylesheet' type='text/css' media='screen' href='css/app.css'>
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+</head>
+
+<body>
+
+  <header class="mb-5">
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top "  style="background-color:#1d1d1b;">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="img/ifglogo.jpeg" width="30%" height="30%" ></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body" style="background-color:#1d1d1b;">
+            <ul class="navbar-nav d-flex justify-content-end flex-grow-1 pe-5  ">
+              	<li class="nav-item ">
+                		<a class="nav-link active px-3 fw-bold" aria-current="page" href="?principal">INÍCIO</a>
+             	 </li>
+                <li class="nav-item">
+                    <a class="nav-link active px-3 fw-bold" aria-current="page" href="?secao=alunoLogin">ENTRAR</a>
+              	</li>
+              	<li class="nav-item">
+                		<a class="nav-link active px-3 fw-bold  " href="adm/index.php">ADMINISTRADOR</a>
+              	</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</br>
+</body>
