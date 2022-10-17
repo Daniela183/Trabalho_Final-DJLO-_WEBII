34e95612768bda6dbaaf525a84bc8e5a3d54487a
João Gabriel Grandotto Viana <63874169+joaograndotto@users.noreply.github.com>
2022-10-17 14:41:37 -0300

    Add files via upload

--- db_trabalhofinal.sql
+++ db_trabalhofinal.sql
@@ -1,25 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- version 5.1.3
+-- httpswww.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Tempo de geração: 26-Jul-2022 às 15:12
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
+-- Host 127.0.0.1
+-- Tempo de geração 27-Jul-2022 às 1529
+-- Versão do servidor 10.4.24-MariaDB
+-- versão do PHP 8.1.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = NO_AUTO_VALUE_ON_ZERO;
 START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+SET time_zone = +0000;
 
 --
--- Banco de dados: `db_trabalhofinal`
+-- Banco de dados `db_trabalhofinal`
 --
+CREATE DATABASE IF NOT EXISTS `db_trabalhofinal` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_trabalhofinal`;
 
 -- --------------------------------------------------------
 
@@ -41,12 +37,8 @@ CREATE TABLE `tb_aluno` (
 --
 
 INSERT INTO `tb_aluno` (`id`, `nome`, `senha`, `matricula`, `cpf`, `email`) VALUES
-(1, 'Afrânio', '12345', '12345', '123.456.999-00', 'afranio@gmail.com'),
-(2, 'Luan', '456', '4567', '789.555.666-17', 'luan@gmail.com'),
-(3, 'joao', '123', '333', '123.123.123-12', 'joao@gmail.com'),
-(4, 'Otávio', '123', '123', '000.001.009-12', 'profeta.katiane@gmail.com'),
-(5, 'Daniela', '123', '20201070', '023.254.135-56', 'vania@gmail.com'),
-(6, 'Arthur', '123', '123321', '123.123.123-19', 'arthur@gmail.com');
+(1, 'joao', '123', '7891027127961', '123.233.123-12', 'joao@gmail.com'),
+(2, 'Luan', '123', '07898931331200', '123.123.123-12', 'teste@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -70,7 +62,8 @@ CREATE TABLE `tb_evento` (
 --
 
 INSERT INTO `tb_evento` (`id`, `titulo`, `descricao`, `dia`, `horarioI`, `horarioF`, `vagas`, `horas`) VALUES
-(1, 'Palestra sobre o laguinho do vovô', 'banho comunitário', '2022-07-27', '08:00:00', '12:00:00', 0, 5);
+(3, 'Palestra Robótica', 'sfdgsd', '21111-03-23', '120300', '120300', 31, 8),
+(6, 'fasdf', 'sfdgsd', '2022-07-06', '085000', '115000', 20, 31);
 
 -- --------------------------------------------------------
 
@@ -83,8 +76,8 @@ CREATE TABLE `tb_evento_aluno` (
   `id_evento` int(11) NOT NULL,
   `id_aluno` int(11) NOT NULL,
   `status` int(11) NOT NULL,
-  `checkin` time DEFAULT NULL,
-  `checkout` time DEFAULT NULL
+  `checkin` time NOT NULL,
+  `checkout` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -92,11 +85,49 @@ CREATE TABLE `tb_evento_aluno` (
 --
 
 INSERT INTO `tb_evento_aluno` (`id`, `id_evento`, `id_aluno`, `status`, `checkin`, `checkout`) VALUES
-(1, 1, 5, 0, '09:39:52', NULL),
-(2, 1, 3, 0, '09:43:13', '09:45:27'),
-(3, 1, 2, 0, NULL, NULL),
-(4, 1, 1, 0, '09:48:18', NULL),
-(5, 1, 4, 0, NULL, NULL);
+(1, 3, 1, 1, '093140', '093152'),
+(2, 6, 1, 0, '000000', '000000'),
+(3, 6, 2, 1, '101756', '101803');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura stand-in para vista `vw_evento`
+-- (Veja abaixo para a view atual)
+--
+CREATE TABLE `vw_evento` (
+`nome` varchar(200)
+,`horas` int(11)
+,`titulo` varchar(200)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura stand-in para vista `vw_nome`
+-- (Veja abaixo para a view atual)
+--
+CREATE TABLE `vw_nome` (
+`nome` varchar(200)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para vista `vw_evento`
+--
+DROP TABLE IF EXISTS `vw_evento`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_evento`  AS SELECT `b`.`nome` AS `nome`, `c`.`horas` AS `horas`, `c`.`titulo` AS `titulo` FROM ((`tb_evento_aluno` `a` left join `tb_aluno` `b` on(`a`.`id_aluno` = `b`.`id`)) left join `tb_evento` `c` on(`a`.`id_evento` = `c`.`id`))  ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para vista `vw_nome`
+--
+DROP TABLE IF EXISTS `vw_nome`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_nome`  AS SELECT DISTINCT `b`.`nome` AS `nome` FROM ((`tb_evento_aluno` `a` left join `tb_aluno` `b` on(`a`.`id_aluno` = `b`.`id`)) left join `tb_evento` `c` on(`a`.`id_evento` = `c`.`id`))  ;
 
 --
 -- Índices para tabelas despejadas
@@ -130,19 +161,19 @@ ALTER TABLE `tb_evento_aluno`
 -- AUTO_INCREMENT de tabela `tb_aluno`
 --
 ALTER TABLE `tb_aluno`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `tb_evento`
 --
 ALTER TABLE `tb_evento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `tb_evento_aluno`
 --
 ALTER TABLE `tb_evento_aluno`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restrições para despejos de tabelas
@@ -155,7 +186,3 @@ ALTER TABLE `tb_evento_aluno`
   ADD CONSTRAINT `fk_aluno` FOREIGN KEY (`id_aluno`) REFERENCES `tb_aluno` (`id`),
   ADD CONSTRAINT `fk_evento` FOREIGN KEY (`id_evento`) REFERENCES `tb_evento` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
